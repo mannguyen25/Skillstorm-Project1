d96201699dbcbcd7d094e87f3d1d45fdb0817b2e
mannguyen25 <nguyenman025@gmail.com>
2022-10-10 06:51:19 -0400

    icon

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import { Box, Typography, Slide } from "@mui/material";
+import { Box, Typography, Slide, Card } from "@mui/material";
 import { useTheme } from "@emotion/react";
 export const Home = () => {
     const theme = useTheme();
@@ -14,9 +14,8 @@ export const Home = () => {
                 mx: 20,
                 textAlign: "left",
                 border: `2px solid ${theme.palette.mode === 'dark'? '#fff': '#000'}`,
-                borderRadius: '3%',
                 padding: 10,
-                boxShadow: `-4px 6px 0px 2px ${theme.palette.mode === 'dark'? '#fff': '#000'}`,   
+                boxShadow: `-10px 10px 0px 2px ${theme.palette.mode === 'dark'? '#fff': '#000'}`,   
             }}
             >
                 <Typography variant="h2">
